860709d8dbfa519806ae8b6daa4cd2497301a457
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-06 16:26:32 +0100

    created the script and the html structure

---
+++ script.js
@@ -0,0 +1,19 @@
+let km = parseInt(prompt('Quanti km vuoi percorrere?'));
+let age = parseInt(prompt('Quanti anni hai?'));
+let price = km * 0.21;
+console.log(price, 'prezzointero');
+let finalprice
+
+if (age <= 17){
+    finalprice = price - ((price * 20) / 100);
+    console.log(finalprice, 'prezzo20');
+} 
+else if(age >= 65){
+    finalprice = price - ((price * 40) / 100);
+    console.log(finalprice, 'prezzo40');
+}
+else{ 
+    finalprice = price
+}
+
+document.getElementById('outputprice').innerHTML = finalprice;
